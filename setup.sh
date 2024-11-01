e3c320f67ccb2cfa9cde2bb1ff5fbf519f3a1cac
Lukas0120 <Lukas.swe@hotmail.com>
2024-11-01 01:08:12 +0100

    lollll

--- setup.sh
+++ setup.sh
@@ -14,6 +14,8 @@ echo "
 # Configuring Best Mirrors & Setting up repos
 ###############################################################################
 "
+sudo pacman -S reflector rsync
+
 sudo reflector -a 48 -c SE -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
 
 
@@ -28,14 +30,38 @@ PKGS=(
 'imagemagick'
 'w3m'
 'rxvt-unicode-truecolor-wide-glyphs'
+'nm-connection-editor'
+'networkmanager'
+'grub-customizer'
+'gparted'
+'lzop'
+'lz4'
+'mimalloc'
+'cpio'
+'unzip'
+'p7zip'
+'geany'
+'geany-plugins'
+'qogir-icon-theme'
+'neofetch'
+'firefox'
+'kitty'
+'linux-tools'
+'yay'
+'paru'
 
 )
 
 for PKG in "${PKGS[@]}"; do
     echo "INSTALLING: ${PKG}"
-    sudo pacman -S "$PKG" --needed --overwrite '*'
+    sudo pacman -S "$PKG" --needed --noconfirm --overwrite '*'
 done
 
+
+mkdir ~/clang
+cd ~/clang
+wget https://github.com/Mandi-Sa/clang/releases/download/amd64-full-toolchain-20/llvm20.0.0-binutils2.42_amd64-full-toolchain-20241001.7z
+7z x amd64-full-toolchain*
 
 echo "
 ###############################################################################
@@ -43,18 +69,22 @@ echo "
 ###############################################################################
 "
 sleep 2
-cd ~
-mkdir ~/build
-cp ~/lulz/build.sh ~/build/
-cd ~
 
+cd ~/lulleman/cachyos-repo/
+sudo ./cachyos-repo.sh
+
+sudo bash -c 'cat << EOF >> /etc/pacman.conf
+[chaotic-aur]
+Server = https://random-mirror.chaotic.cx/\$repo/\$arch
+SigLevel = Never
+#wailord284 custom repo with many aur packages used by Alex's Arch Linux Installer
+[aurmageddon]
+Server = https://wailord284.club/repo/\$repo/\$arch
+SigLevel = Never
+EOF'
 
 
 xrdb ~/.Xresources
-
-sudo modprobe -r k10temp
-sudo bash -c 'sudo echo -e "\n# replaced with zenpower\nblacklist k10temp" >> /etc/modprobe.d/blacklist.conf'
-sudo modprobe zenpower
 
 
 sudo ln -sf /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
